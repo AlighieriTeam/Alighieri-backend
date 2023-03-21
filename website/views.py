8e2e6f4c3296aad77c44b1b55a55d5878e6c4fe7
Mateusz Pruss <235953@edu.p.lodz.pl>
2023-03-21 21:10:29 +0100

    refactor: changed names of files and connections to fit figma naming

--- website/views.py
+++ website/views.py
@@ -2,18 +2,19 @@ from flask import Blueprint, render_template, request
 
 views = Blueprint('views', __name__)
 @views.route('/')
+@views.route('/home')
 def home_page():
 
     return render_template("home.html")
 
-@views.route('/new-game')
-def new_game():
+@views.route('/room')
+def room():
     from .game import games, Game
 
-    return render_template("new-game.html", games=games)
+    return render_template("room.html", games=games)
 
-@views.route('/choose-game', methods=['GET', 'POST'])
-def choose_game():
+@views.route('/choose', methods=['GET', 'POST'])
+def choose():
     if request.method =='POST':
         from . import active_games
         pin = request.form.get('pin')
@@ -21,8 +22,8 @@ def choose_game():
         # if game does not exist
         if len(game_list) == 0:
             # TODO: throw error with flash
-            render_template("choose-game.html")
+            render_template("choose.html")
 
         # TODO: redirect to game page
         pass
-    return render_template("choose-game.html")
+    return render_template("choose.html")
