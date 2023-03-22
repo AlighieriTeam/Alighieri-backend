0c8eda66d28900217e94ab0dcfa9ab90a8bb0c73
Mateusz Pruss <235953@edu.p.lodz.pl>
2023-03-22 01:07:39 +0100

    feat: #19 added game.html
    + working timer to game.html (redirecting -> choose)
    + simple Player class to test automatically generated players in game.html

--- website/game.py
+++ website/game.py
@@ -1,6 +1,14 @@
 
 
 games = ['pac-man', 'pong']
+
+class Player:   # simple player class to show players in game page automatically
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+players = [Player(1, "Jacek"), Player(2, "Placek"), Player(3, "Yan"), Player(4, "Covalaki")]
+
 class Game:
     def __init__(self, pin : str, game_type : str):
         assert 3 < len(pin) < 10
