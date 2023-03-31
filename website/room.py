98a776444652b4e4d7ed4a36b6c643cf6dd6691d
krizzuu <kskrzysiek788@gmail.com>
2023-03-31 17:19:35 +0200

    refactor: renamed game types constant

--- website/room.py
+++ website/room.py
@@ -1,6 +1,5 @@
 
-
-games = ['pac-man', 'pong']
+GAME_TYPES = ['pac-man', 'pong']
 
 class Player:   # simple player class to show players in game page automatically
     def __init__(self, id, name):
@@ -12,7 +11,7 @@ players = [Player(1, "Jacek"), Player(2, "Placek"), Player(3, "Yan"), Player(4, 
 class Room:
     def __init__(self, pin : str, game_type : str):
         assert 3 < len(str(pin)) < 10
-        assert game_type in games
+        assert game_type in GAME_TYPES
 
         self.pin = pin
         self.game_type = game_type
