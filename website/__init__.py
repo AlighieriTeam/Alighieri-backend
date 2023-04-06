ca0c5686731e542d7fd8350a20758bea67438848
Krizzuu <47757676+Krizzuu@users.noreply.github.com>
2023-04-06 20:12:01 +0200

    feat: now using socketio

--- website/__init__.py
+++ website/__init__.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from typing import Optional
+
+from flask import Flask, session
+from flask_socketio import join_room, leave_room, send, SocketIO
 from .room import Room
 
-active_games: list[Room] = []
+rooms = {}
+
+
+def find_game(pin: str) -> Optional[Room]:
+    return rooms.get(pin)
+
+
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'secret!'
+    socketio = SocketIO(app)
 
     from .views import views
 
     app.register_blueprint(views, url_prefix='/')
 
-    return app
+    @socketio.on("connect")
+    def connect(auth):
+        pass
+
+    return app, socketio
