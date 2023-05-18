97b65605b503d2d375cf8e92ef7cebe999fb7772
Mateusz Pruss <235953@edu.p.lodz.pl>
2023-05-18 23:49:17 +0200

    refactor: move JS code from HTML to external JS files
    Everything works as before :)

---
+++ website/static/scripts/join-events.js
@@ -0,0 +1,35 @@
+// ------------ socketIO signals for join.html ------------
+
+socketio.on('kick', function(json) {
+    if(json["id"] == actual_player_id){
+        window.location.href = json["dest"];
+    }
+    else{
+        delPlayer(json["id"])
+    }
+});
+
+
+
+// ------------ JS functions for join.html ------------
+
+function refreshPlayerList() {
+    const playerList = document.getElementById("player-list");
+    let content = "";
+    for (const [id, name] of playerMap.entries()) {
+        content += `
+            <div id="player_${id}" class="player_div player_gradient_${id}">
+                <div class="ls-player-name">${name}</div>
+                <div class="ls-player-del"></div>
+            </div>
+        `
+    }
+    playerList.innerHTML = content;
+}
+
+function delPlayer(player_id){
+    const playerList = document.getElementById("player-list");
+    playerList.innerHTML = "";
+    playerMap.delete(player_id);
+    refreshPlayerList();
+}
